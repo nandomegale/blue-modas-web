8a28282725936fd917e65a55ef15772722ceded3
Luiz Fernando Megale Paiva <luiz_paiva@gec.inatel.br>
2021-03-02 17:58:56 -0300

    arrumando variaveis de producao

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+const mainURL: string = "https://blue-modas-web.herokuapp.com"
+
 export const environment = {
-  production: true
+  production: true,
+  product: {
+    productEndPoint: `${mainURL}products`
+  },
+  order: {
+    orderEndPoint: `${mainURL}orders`
+  }
 };
