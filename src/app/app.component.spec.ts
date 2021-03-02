86e03e77b396339e8e2f07df74c3999752055b17
Luiz Fernando Megale Paiva <luiz_paiva@gec.inatel.br>
2021-03-02 17:34:49 -0300

    preparando deploy no heroku

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'blue-modas'`, () => {
+  it(`should have as title 'blue-modas-web'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('blue-modas');
+    expect(app.title).toEqual('blue-modas-web');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('blue-modas app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('blue-modas-web app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blue-modas';
+  title = 'blue-modas-web';
 
   cart = JSON.parse(sessionStorage.getItem('cart'));
   cartQuantity = this.cart ? this.cart.length : 0; //se inscrever num observable
